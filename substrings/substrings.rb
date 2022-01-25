ecf6b4e35252b58e514372584741caf5f96c6c8c
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-01-24 19:38:24 -0600

    Create function to count if a substring apppears in the text

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,3 +1,10 @@
-def substrings(text, substrings) {
-  
-}
+def substrings(text, substrings) 
+  text.split.reduce(Hash.new(0)) do |result, word|
+    substrings.each do |substr|
+      if word.match?(substr)
+        result[substr] += 1
+      end
+    end
+    result
+  end
+end
