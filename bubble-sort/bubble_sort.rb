2f0ac4e847bdd27fd1b474aa961766172232caf1
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-01-25 23:57:11 -0600

    Merge all loops and comparations in one single loop

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -1,28 +1,15 @@
-def sort_pair(pair)
-  a = pair[0]
-  b = pair[1]
-  is_b_greater = (a <=> b) > 0
-  return is_b_greater ? pair.reverse : pair
-end
-
-def bubble_sort(array)
-  result = array
-  until is_sorted?(result)
-    result[0..-2].each_with_index do |val, index|
-      sorted = sort_pair(result[index..index+1])
-      result[index] = sorted[0]
-      result[index+1] = sorted[1]
-    end
-  end
-  result
-end
-
-def is_sorted? (array)
+def new_bubble_sort(array)
   max_len = array.length - 2
-  for i in 0..max_len do
-    if array[i] > array[i+1]
-      return false
+  sorted = false
+  
+  until sorted do
+    sorted = true
+    for i in 0..max_len do
+      if array[i] > array[i+1]
+        sorted = false
+        array[i], array[i+1] = array[i+1], array[i]
+      end
     end
   end
-  return true
+  array  
 end
