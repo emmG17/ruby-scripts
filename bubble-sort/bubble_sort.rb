39e2ad776afb2ed67ebdaf3ebb1086f5d13eb5c8
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-01-26 00:02:49 -0600

    Add explaining comments

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -1,13 +1,13 @@
 def bubble_sort(array)
-  max_len = array.length - 2
+  max_len = array.length - 2 # Set it to the penultimate index to avoid nil values
   sorted = false
   
   until sorted do
     sorted = true
     for i in 0..max_len do
-      if array[i] > array[i+1]
+      if array[i] > array[i+1] # Compare neighboring items
         sorted = false
-        array[i], array[i+1] = array[i+1], array[i]
+        array[i], array[i+1] = array[i+1], array[i] # Swap items to sort them
       end
     end
   end
